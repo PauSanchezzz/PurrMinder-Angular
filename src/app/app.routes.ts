b05c6298d0e18daf555e916603047470448f5cb9
PaulaSanchez <pausanchezt12@gmail.com>
2024-07-20 16:14:38 -0500

    new routes added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import AppRecomendationsComponent from './components/recomendations/app.recomendations/app.recomendations.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'recomendations',
+    component: AppRecomendationsComponent,
+  },
+];
