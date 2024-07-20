736b423521724b58944e300c08c9c1a4894e668e
PaulaSanchez <pausanchezt12@gmail.com>
2024-07-19 19:04:04 -0500

    new components added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { OurServicesSectionComponent } from './components/our-services-section/our-services-section.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    OurServicesSectionComponent,
+    FooterComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
