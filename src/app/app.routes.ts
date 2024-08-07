a05d4ddbccc5ca3309d121d4c82756773e765874
PaulaSanchez <pausanchezt12@gmail.com>
2024-08-07 11:58:32 -0500

    added cat registration form with responsive design.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,7 @@ import { AdopteesComponent } from './pages/adoptees/adoptees.component';
 import { HomeComponent } from './pages/home/home.component';
 import AppRecomendationsComponent from './pages/recomendations/app.recomendations.component';
 import { AdoptionsComponent } from './pages/adoptions/adoptions.component';
+import { CatRegisterComponent } from './pages/cat-register/cat-register.component';
 
 export const routes: Routes = [
   {
@@ -40,5 +41,9 @@ export const routes: Routes = [
   {
 path: 'adoptees',
 component: AdopteesComponent,
+  },
+  {
+    path: 'catRegister',
+    component: CatRegisterComponent,
   }
 ];
---
+++ src/app/pages/cat-register/cat-register.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../../components/header/header.component';
+
+@Component({
+  selector: 'app-cat-register',
+  standalone: true,
+  imports: [HeaderComponent],
+  templateUrl: './cat-register.component.html',
+  styleUrl: './cat-register.component.css'
+})
+export class CatRegisterComponent {
+
+}
